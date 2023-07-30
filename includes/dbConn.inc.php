6fbf7af5a834a5498c0fd460ce1c496da9b67300
Kasun-Abeywickrama <121012573+Kasun-Abeywickrama@users.noreply.github.com>
2023-07-30 20:03:29 +0530

    Edit Item page related pages

--- includes/dbConn.inc.php
+++ includes/dbConn.inc.php
@@ -1,17 +1,13 @@
 <?php 
     // Database credentials
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "e-commerce-db";
+    $serverName = "localhost";
+	$userName = "root";
+	$password = "";
+	$database = "e_commerce_db";
 
-    //Connection String to Connect to the database
-    $conn = new mysqli($host, $username, $password, $database);
-
-    // Check if the connection was successful
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }else{
-        echo "<script>console.log('Database connection success');</script>";
-    }
+	$conn = new mysqli($serverName, $userName, $password, $database);
+	if($conn->connect_error)
+	{
+		die("Database connection failed".$conn->connect_error);
+	}
 ?>
